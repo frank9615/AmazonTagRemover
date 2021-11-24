73dc724fbca8f606b659ba452e68ac6b92702cd5
frank9615 <accardifrancesco@hotmail.it>
2021-11-24 23:50:41 +0100

    Fix #1

--- src/antitag.js
+++ src/antitag.js
@@ -1,21 +1,43 @@
-window.addEventListener("load", () => {
-    let reload = false;
-    let href = window.location.href;
-    const regextag = /tag=.*/g;
-    const found1 = href.match(regextag);
-    if (found1 && found1.length > 0) {
-        href = href.replace(regextag, "");
-        reload = true;
+// # see https://developer.chrome.com/docs/extensions/reference/webRequest/#registering-event-listeners
 
-    }
-    const regexlinkcode = /linkCode=.*/g;
-    const found2 = href.match(regexlinkcode);
-    if (found2 && found2.length > 0) {
-        href = href.replace(regexlinkcode, "");
-        reload = true;
-    }
+const opt_extraInfoSpec = ['blocking'];
+const filter = {
+    urls: [
+        "*://*.amazon.ca/*",
+        "*://*.amazon.co.jp/*",
+        "*://*.amazon.co.uk/*",
+        "*://*.amazon.com.au/*",
+        "*://*.amazon.com.br/*",
+        "*://*.amazon.com.mx/*",
+        "*://*.amazon.com/*",
+        "*://*.amazon.de/*",
+        "*://*.amazon.es/*",
+        "*://*.amazon.fr/*",
+        "*://*.amazon.in/*",
+        "*://*.amazon.it/*",
+        "*://*.amazon.nl/*",
+        "*://*.amzn.com/*"
+    ]
+};
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        let redirect = false;
+        let url = details.url;
+        const regextag = /tag=.*/g;
+        const found1 = url.match(regextag);
+        if (found1 && found1.length > 0) {
+            url = url.replace(regextag, "");
+            redirect = true;
+        }
+        const regexlinkcode = /linkCode=.*/g;
+        const found2 = url.match(regexlinkcode);
+        if (found2 && found2.length > 0) {
+            url = url.replace(regexlinkcode, "");
+            redirect = true;
+        }
+        if (redirect) {
+            return { redirectUrl: url };
+        }
+    }, filter, opt_extraInfoSpec);
 
-    if (reload) {
-        window.location.href = href;
-    }
-});
+
